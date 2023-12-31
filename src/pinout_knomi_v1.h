0b6de3a57dd44cb8cc589ca76399c8bb4ab5db9d
Thijs Triemstra <info@collab.nl>
2023-12-31 04:49:36 +0100

    update pins

--- src/pinout_knomi_v1.h
+++ src/pinout_knomi_v1.h
@@ -1,16 +1,19 @@
 #ifndef PINOUT_KNOMI_V1_H
 #define PINOUT_KNOMI_V1_H
 
-#define BOOT_PIN     0
+#define BOOT_PIN        0
 
 // knomi v1
-#define GC9A01_MOSI_PIN 23 // In some display driver board, it might be written as "SDA" and so on.
-#define GC9A01_SCLK_PIN 18
-#define GC9A01_CS_PIN   5  // Chip select control pin
-#define GC9A01_DC_PIN   19  // Data Command control pin
-#define GC9A01_RST_PIN  4  // Reset pin (could connect to Arduino RESET pin)
+#define GC9A01_MOSI_PIN 11 // In some display driver board, it might be written as "SDA" and so on.
+#define GC9A01_SCLK_PIN 13
+#define GC9A01_CS_PIN   10 // Chip select control pin
+#define GC9A01_DC_PIN   12 // Data Command control pin
+#define GC9A01_RST_PIN  14 // Reset pin (could connect to Arduino RESET pin)
 
-// // PWM
-#define LCD_BL_PIN         2
+// LED
+#define RGB_LED_PIN     48
+
+// PWM
+#define LCD_BL_PIN      20
 
 #endif
